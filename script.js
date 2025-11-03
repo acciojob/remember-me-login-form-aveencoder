561067bb2bf54c8927823bbc785bb5f6524693c0
aveencoder <aveen598@gmail.com>
2025-11-03 13:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+   window.addEventListener('DOMContentLoaded', () => {
+      const usernameInput = document.getElementById('username');
+      const passwordInput = document.getElementById('password');
+      const checkbox = document.getElementById('checkbox')
+      const existingBtn = document.getElementById('existing');
+	  const saved = localStorage.getItem('savedCredentials');
+
+	   if(saved){
+		    existingBtn.style.removeProperty( "display");
+	   }
+	   
+   })
