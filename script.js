2623119f36dd9ece4f11b1c497647848df2d580d
aveencoder <aveen598@gmail.com>
2025-11-04 00:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,25 @@
       const checkbox = document.getElementById('checkbox')
       const existingBtn = document.getElementById('existing');
 	  const saved = localStorage.getItem('savedCredentials');
+	  const submit = document.getElementById('submit');
 
 	   if(saved){
-		    existingBtn.style.removeProperty( "display");
+		    existingBtn.style.removeProperty("display");
 	   }
+
+	
+	  submit.addEventListener('click',()=>{
+		  e.preventDefault();
+	const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+		   if(!username || !password){
+			alert('Please enter username and password!');
+            return;
+		   }
+		  else{
+			  alert("Logged in as <username>");
+		  }
+	  }) 
+	   
 	   
    })
